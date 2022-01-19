af2083d3236c8c4b5b29d72e45209920b631b408
Achilles Rasquinha <achillesrasquinha@gmail.com>
2022-01-19 12:27:08 -0600

    feat: Initial Commit

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,2 +1,15 @@
-
 from __future__ import absolute_import
+
+from bot.connection import Connection
+
+class Bot:
+    def connect(self):
+        self.connection = Connection()
+
+    def __enter__(self):
+        self.connect()
+        return self
+
+    def __exit__(self):
+        self.disconnect()
+        return self
